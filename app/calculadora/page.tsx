995b02f990e958a72258ee153734365161c0c6c3
Breno Balsini <balsini.breno@gmail.com>
2026-01-20 20:10:20 -0300

    Calculadora PlaceHolder

---
+++ app/calculadora/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Calculadora() {
+  
+  return (
+    <main className="p-4">
+      <h1 className="text-2xl font-bold">Flor de Sal</h1>
+      <p className="text-sm text-gray-600">Calculadora</p>
+    </main>
+  );
+}
